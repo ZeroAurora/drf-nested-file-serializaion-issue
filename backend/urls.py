bfaf620edaeaefa12f7c59235644be3e3cb4f758
ZeroAurora <25359455+ZeroAurora@users.noreply.github.com>
2025-01-11 09:17:45 +0800

    add media url

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework import routers
 from file.views import MediaFileViewSet, InnerViewSet, OuterViewSet
@@ -29,4 +31,4 @@ router.register(r'outer', OuterViewSet, basename='outer')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
